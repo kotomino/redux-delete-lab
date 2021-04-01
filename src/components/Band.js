7c4e507f2d8a9eab6307664f4a284f9116afa3f0
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2021-04-01 19:35:23 -0400

    commit tree 50039e08608c3583979b5d79547b5bf4f5a19c57

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleOnClick = e => {
+    this.props.deleteBand(this.props.id)
+  }
+
   render() {
     return(
       <div>
-        Band Component
+        <li>{ this.props.name }</li>
+        <button onClick={this.handleOnClick}>
+          Delete
+        </button>
       </div>
     );
   }
